116b7c6f58a82eafa7fee052b5ffd6ed1a6dc404
Jun Tan <tanjunyuan@hotmail.co.uk>
2017-08-02 11:06:34 -0500

    fix errors

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import UserMeta, Category, Post
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
 
 # Create your views here.
 def homepage(request):
